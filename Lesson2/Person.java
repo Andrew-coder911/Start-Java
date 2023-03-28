4269c93c0e3bbd3a2e8ee4c6f46d90355654eb58
Andrew Galkin <dalamar132@mail.ru>
2023-03-28 22:39:32 +0300

    ДЗ1 урок 2
    - Создание класса человек

---
+++ Lesson2/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    String male = "Мужской";
+    String name = "Андрей";
+    int height = 183;
+    int weight = 74;
+    int age = 31;
+    int speed = 5;
+
+    //как правильно менять скорость объекта?
+    public void run(int speed) {
+        speed = 8;
+    }
+    //тут поменяется скорость объекта?
+    public void walk(int speed) {
+        this.speed = 5;
+    }
+    //Или так?
+    public void stay() {
+        this.speed = 0;
+    }
+
+    public void say() {
+        System.out.println("Что-то сказал");
+    }
+}
